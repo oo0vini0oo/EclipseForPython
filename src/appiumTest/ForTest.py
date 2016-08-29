b795aea4a85d3ea3aaf397857ecb7b6b5f683a61
oo0vini0oo <vini_mr.w@163.com>
2016-08-29 16:06:27 +0800

    顺序执行myunit中的用例

--- src/appiumTest/ForTest.py
+++ src/appiumTest/ForTest.py
@@ -20,23 +20,35 @@ class Test(unittest.TestCase):
         pass
 
     @classmethod
-    def testName(self):
-        # findText("下次不再提醒")
+    def testFindText(self):
+        findText("下次不再提醒")
+        time.sleep(2)       
+       
+    @classmethod
+    def testClickText(self):
         clickText("下次不再提醒")
         time.sleep(2)
         clickText("下次不再提醒")
         time.sleep(2)
         clickText("同 意")
-        time.sleep(2) 
+        time.sleep(2)    
+        
+    @classmethod
+    def testClickResourceID(self):
         # 如何做到自动遍历当前界面所有控件并依次点击？
         clickResourceID("net.easyconn.carman:id/iv_user")
-        
+       
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
+    # 设置一个容器来调用将要执行的测试用例
     myunit =unittest.suite.TestSuite()
-    myunit.addTest(Test("testName"))
-    mytime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    # 需要进行测试的用例，顺序执行
+    myunit.addTest(Test("testClickText"))
+    myunit.addTest(Test("testFindText"))    
+    myunit.addTest(Test("testClickResourceID"))
+    # 获取当前系统时间
+    # mytime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
     myfile="C:/Users/Administrator/Documents/a.html"
     fo = open(myfile,"wb")
     runner=HTMLTestRunner.HTMLTestRunner(

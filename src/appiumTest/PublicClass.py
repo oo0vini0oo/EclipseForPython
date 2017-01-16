0125f4c3a590fb9bfba33aec61e423b6aa473fc6
willie <willie@wangweili>
2017-01-16 15:06:23 +0800

    修改文字提示

--- src/appiumTest/PublicClass.py
+++ src/appiumTest/PublicClass.py
@@ -77,34 +77,34 @@ def getScreenShot(filename):
     myscreenshot="C:/Users/willie/Documents/"+mytime+filename+".png"
     driver.get_screenshot_as_file(myscreenshot)
 
-#获得机器屏幕大小x,y
+# 获得机器屏幕大小x,y
 def getSize():
     x = driver.get_window_size()['width']
     y = driver.get_window_size()['height']
     return (x, y)
  
-#屏幕向上滑动
+# 屏幕向上滑动
 def swipeUp(duration):
     l = getSize()
     x1 = int(l[0] * 0.5)  #x坐标
     y1 = int(l[1] * 0.75)   #起始y坐标
     y2 = int(l[1] * 0.25)   #终点y坐标
     driver.swipe(x1, y1, x1, y2,duration)
-#屏幕向下滑动
+# 屏幕向下滑动
 def swipeDown(duration):
     l = getSize()
     x1 = int(l[0] * 0.5)  #x坐标
     y1 = int(l[1] * 0.25)   #起始y坐标
     y2 = int(l[1] * 0.75)   #终点y坐标
     driver.swipe(x1, y1, x1, y2,duration)
-#屏幕向左滑动
+# 屏幕向左滑动
 def swipeLeft(duration):
     l=getSize()
     x1=int(l[0]*0.75)
     y1=int(l[1]*0.5)
     x2=int(l[0]*0.05)
     driver.swipe(x1,y1,x2,y1,duration)
-#屏幕向右滑动
+# 屏幕向右滑动
 def swipeRight(duration):
     l=getSize()
     x1=int(l[0]*0.05)
